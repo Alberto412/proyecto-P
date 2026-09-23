cdcfd6dd66a0d2aed5ee50ce2a6d9bc0ac046e0c
Alberto412 <anavmol2011@g.educaand.es>
2026-09-23 11:05:48 +0200

    generador sandbox inicios

--- src/generation/GeneradorSandbox.cpp
+++ src/generation/GeneradorSandbox.cpp
@@ -3,3 +3,13 @@
 //
 
 #include "GeneradorSandbox.h"
+std::vector<Posicion> GeneradorSandbox::crearPosicionesDisponibles(
+    int ancho,
+    int alto) {
+    std::vector<Posicion> posiciones;
+    for (int y; y< alto;y++) {
+        for (int x; x<ancho;x++) {
+            posiciones.push_back(Posicion{x,y});
+        }
+    }
+}
--- src/generation/GeneradorSandbox.h
+++ src/generation/GeneradorSandbox.h
@@ -5,8 +5,8 @@
 #ifndef PROYECTOP_GENERADORSANDBOX_H
 #define PROYECTOP_GENERADORSANDBOX_H
 
-#import <vector>
-#include "/world/Posicion.h"
+#include  <vector>
+#include "world/Posicion.h"
 class GeneradorSandbox {
 private:
     std::vector<Posicion> crearPosicionesDisponibles(int ancho, int alto);
